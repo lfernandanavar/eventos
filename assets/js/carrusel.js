14c6bbca24fdf75f4304ad8d04b747a504533587
lfernandanavar <lfernandanavar@gmail.com>
2023-09-15 23:28:00 -0600

    first commit

---
+++ assets/js/carrusel.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $('#carousel-example-generic').carousel({
+        interval: 10000
+    });
+    
+    console.log('hello World');
+        
+    $('#carousel-example-generic .item').each(function() {
+        var next = $(this).next();
+        
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
